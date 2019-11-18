45de94749d45429bdbf82c9222d8ed70bdbf5efa
chenyanyang <184536632@qq.com>
2019-11-18 08:06:04 +0800

    update

--- control_theory/ver_de_pol/growingvdp.py
+++ control_theory/ver_de_pol/growingvdp.py
@@ -5,15 +5,30 @@ import numpy as np
 
 
 fig = plt.figure() 
-# ax = plt.axes(xlim=(0, 500), ylim=(-3, 4)) 
-ax = plt.axes(xlim=(-4, 5), ylim=(-3, 4)) 
+# ax = plt.axes(xlim=(0, 500), ylim=(-10, 10)) 
+ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10)) 
 line, = ax.plot([], [], lw=2) 
 
 mu = 0.2
 
 def van_der_pol_oscillator_deriv(x, t):
     nx0 = x[1]
-    nx1 = -mu * (x[0] ** 2.0 - 1.0) * x[1] - x[0]
+    # nx1 = -mu * (x[0] ** 2.0 - 1.0) * x[1] - x[0]
+    nx1 = -mu * x[0] ** 2.0 - 1.0 * x[1] - x[0]
+    res = np.array([nx0, nx1])
+    return res
+
+def vunderwater_vehicle(x, t):
+    nx0 = x[1]
+    # nx1 = -mu * (x[0] ** 2.0 - 1.0) * x[1] - x[0]
+    nx1 = x[1] + x[0]*abs(x[0])
+    res = np.array([nx0, nx1])
+    return res
+
+def second_order_oscillator_deriv(x, t):
+    nx0 = x[1]
+    # nx1 = -mu * (x[0] ** 2.0 - 1.0) * x[1] - x[0]
+    nx1 = -1 * x[0] ** 2.0 - 0.6 * x[1] - 3*x[0]
     res = np.array([nx0, nx1])
     return res
 
@@ -22,7 +37,7 @@ ts = np.linspace(0.0, 50.0, 500)
 # xs = odeint(van_der_pol_oscillator_deriv, [0.2, 0.2], ts)
 # xs = odeint(van_der_pol_oscillator_deriv, [4.0, 4.0], ts)
 # plt.plot(xs[:,0], xs[:,1])
-xs = odeint(van_der_pol_oscillator_deriv, [-3.0, -3.0], ts)
+xs = odeint(second_order_oscillator_deriv, [2.5,0], ts)
 # plt.plot(xs[:,0], xs[:,1])
 
 # initialization function 
@@ -59,5 +74,5 @@ anim = animation.FuncAnimation(fig, animate, init_func=init,
 							frames=500, interval=20, blit=True) 
 
 # save the animation as mp4 video file 
-#anim.save('coil.html',writer='ffmpeg') 
-plt.show()
+anim.save('3.2.mp4',writer='ffmpeg') 
+# plt.show()
